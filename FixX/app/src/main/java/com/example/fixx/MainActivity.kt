fd5f9c53c04bdb54f583505d871b0af3079efd64
moutaz-hegazy <moutazhegazy93@gmail.com>
2021-05-06 00:19:09 +0200

    version 0.0.0
    just connected to firebase

---
+++ FixX/app/src/main/java/com/example/fixx/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.fixx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+
+    var emailEditTxt : EditText? = null
+    var passwordTxt : EditText? = null
+    var signInBtn : Button? = null
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        emailEditTxt = findViewById(R.id.main_email_txt)
+        passwordTxt = findViewById(R.id.main_password_txt)
+        signInBtn = findViewById(R.id.main_signIn_btn)
+
+        auth = Firebase.auth
+
+        signInBtn?.let {
+            it.setOnClickListener { event ->
+                val email = emailEditTxt?.text.toString() ?: ""
+                val password = passwordTxt?.text.toString() ?: ""
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.i("TAG", "createUserWithEmail:success")
+                                val user = auth.currentUser
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w("TAG", "createUserWithEmail:failure", task.exception)
+                                Toast.makeText(baseContext, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show()
+                            }
+                        }
+            }
+        }
+
+    }
+}
