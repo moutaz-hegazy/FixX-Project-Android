c17ad5c7e880fe53e920fa7d746d055f6c5a3ff4
Dina-Adel-Fekry <77211164+Dina-Adel-Fekry@users.noreply.github.com>
2021-05-11 02:53:01 -0700

    Home Screen

---
+++ FixX/app/src/main/java/com/example/fixx/NavigationBar/HomeScreen/POJOs/ServiceItem.kt
@@ -0,0 +1,13 @@
+package com.example.project
+
+class ServiceItem {
+
+    var icons : Int ? = 0
+    var name : String ? = null
+
+
+    constructor(icons: Int?, name: String?) {
+        this.icons = icons
+        this.name = name
+    }
+}
