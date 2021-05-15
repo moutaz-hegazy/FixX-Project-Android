cd8cdf26f6cf8e61ba14f408cae82a729d918c6f
moutaz-hegazy <moutazhegazy93@gmail.com>
2021-05-15 23:29:42 +0200

    zero version 0.0.1

--- FixX/app/src/main/java/com/example/fixx/SignUpOnBoardingFragment.kt
+++ FixX/app/src/main/java/com/example/fixx/SignUpOnBoardingFragment.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import com.example.fixx.POJOs.Details
 import java.util.regex.Pattern
 
 class SignUpOnBoardingFragment : Fragment() {
--- FixX/app/src/main/java/com/example/fixx/SignUpTabFragment.kt
+++ FixX/app/src/main/java/com/example/fixx/SignUpTabFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.example.fixx.POJOs.Details
 import java.util.regex.Pattern
 
 class SignUpTabFragment: Fragment() {
