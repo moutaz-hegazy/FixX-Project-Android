6e6edf3617cbeb22cb0456bca3b57c10d8e66ba4
Dina-Adel-Fekry <77211164+Dina-Adel-Fekry@users.noreply.github.com>
2021-05-22 07:33:41 -0700

    help-edit-language

--- FixX/app/src/main/java/com/example/fixx/NavigationBar/SettingsScreen/ProfileActivity.kt
+++ FixX/app/src/main/java/com/example/fixx/NavigationBar/SettingsScreen/ProfileActivity.kt
@@ -1,23 +1,46 @@
 package com.example.fixx.NavigationBar.SettingsScreen
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.fixx.R
+import com.example.fixx.constants.Constants
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.bottom_sheet_edit_email.*
+import kotlinx.android.synthetic.main.bottom_sheet_edit_name.*
+import kotlinx.android.synthetic.main.bottom_sheet_edit_password.*
+import kotlinx.android.synthetic.main.bottom_sheet_edit_phone.*
+import kotlinx.android.synthetic.main.bottom_sheet_pick.*
 
 class ProfileActivity : AppCompatActivity() {
     var floatingActionButton: FloatingActionButton? = null
     var bottomSheet : BottomSheetDialog?= null
-    private val pickImage = 100
+    val REQUEST_CODE = 100
+
+
+    private val cameraRequest = 1888
+    private var imageUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
         supportActionBar?.hide()
+
+
+
         floatingActionButton = findViewById(R.id.profile_camera_floatingactionbutton)
         floatingActionButton?.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View) {
@@ -27,25 +50,185 @@ class ProfileActivity : AppCompatActivity() {
         }
 
         )
+
+
+
+
+        profile_edit_name_image_button.setOnClickListener {
+            showBottomSheetEditName()
+        }
+
+        profile_edit_email_image_button.setOnClickListener {
+            showBottomSheetEditEmail()
+        }
+
+        profile_edit_phone_image_button.setOnClickListener {
+            showBottomSheetEditPhone()
+        }
+
+        profile_edit_password_image_button.setOnClickListener {
+            showBottomSheetEditPassword()
+        }
+
+
+
+
+
 //        val galleryLinearLayout = findViewById(R.id.profile_gallery_linear_layout) as LinearLayout
 //        galleryLinearLayout.setOnClickListener(object : View.OnClickListener {
 //            override fun onClick(v: View) {
 //
-//                openGallery();
+//                val intent = Intent("MediaStore.ACTION_IMAGE_CAPTURE")
+//            startActivityForResult(intent, 1)
 //            }
 //        })
     }
 
-    fun showBottomSheetDialog(){
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            imageUri = data?.data
+            profile_profilepicture_image_view.setImageURI(imageUri)
+        }
+    }
+
+   private fun showBottomSheetDialog(){
         val btnsheet = layoutInflater.inflate(R.layout.bottom_sheet_pick, null)
+
         val dialog = BottomSheetDialog(this)
         dialog.setContentView(btnsheet)
         btnsheet.setOnClickListener {
             dialog.dismiss()
         }
+       setContentView(R.layout.bottom_sheet_pick)
+
+       profile_gallery_linear_layout.setOnClickListener {
+           val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+           startActivityForResult(gallery, Constants.galleryPickerRequestCode)
+       }
+
+       if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA)
+           == PackageManager.PERMISSION_DENIED)
+           ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), cameraRequest)
+       profile_camera_linear_layout.setOnClickListener {
+           val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+           startActivityForResult(cameraIntent, Constants.cameraPickerRequestCode)
+       }
+
+
+
+//       profile_gallery_linear_layout.setOnClickListener {
+//
+//           val intent = Intent(Intent.ACTION_PICK)
+//           intent.type = "image/*"
+//           startActivityForResult(intent, REQUEST_CODE)
+//       }
         dialog.show()
     }
 
+   private fun showBottomSheetEditName(){
+
+        val btnsheet = layoutInflater.inflate(R.layout.bottom_sheet_edit_name, null)
+
+        val dialog = BottomSheetDialog(this)
+        dialog.setContentView(btnsheet)
+        btnsheet.setOnClickListener {
+            dialog.dismiss()
+
+            bottom_sheet_edit_name_cancel_button.setOnClickListener {
+                   dialog.dismiss()
+            }
+
+            bottom_sheet_edit_name_save_button.setOnClickListener {
+
+                updateName(bottom_sheet_edit_name_edit_text.text.toString())
+                dialog.dismiss()
+            }
+        }
+        dialog.show()
+
+    }
+
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//       super.onActivityResult(requestCode, resultCode, data)
+//       if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE){
+//            profile_profilepicture_image_view.setImageURI(data?.data) // handle chosen image
+//        }
+//   }
+
+
+    private fun updateName(newName : String){
+
+    }
+
+    private fun showBottomSheetEditEmail(){
+
+        val btnsheet = layoutInflater.inflate(R.layout.bottom_sheet_edit_email, null)
+
+        val dialog = BottomSheetDialog(this)
+        dialog.setContentView(btnsheet)
+        btnsheet.setOnClickListener {
+            dialog.dismiss()
+
+            bottom_sheet_edit_email_cancel_button.setOnClickListener {
+                dialog.dismiss()
+            }
+
+            bottom_sheet_edit_email_save_button.setOnClickListener {
+
+                updateName(bottom_sheet_edit_email_edit_text.text.toString())
+                dialog.dismiss()
+            }
+        }
+        dialog.show()
+
+    }
+
+    private fun showBottomSheetEditPhone(){
+
+        val btnsheet = layoutInflater.inflate(R.layout.bottom_sheet_edit_phone, null)
+
+        val dialog = BottomSheetDialog(this)
+        dialog.setContentView(btnsheet)
+        btnsheet.setOnClickListener {
+            dialog.dismiss()
+
+            bottom_sheet_edit_phone_cancel_button.setOnClickListener {
+                dialog.dismiss()
+            }
+
+            bottom_sheet_edit_phone_save_button.setOnClickListener {
+
+                updateName(bottom_sheet_edit_phone_edit_text.text.toString())
+                dialog.dismiss()
+            }
+        }
+        dialog.show()
+
+    }
+
+    private fun showBottomSheetEditPassword(){
+
+        val btnsheet = layoutInflater.inflate(R.layout.bottom_sheet_edit_password, null)
+
+        val dialog = BottomSheetDialog(this)
+        dialog.setContentView(btnsheet)
+        btnsheet.setOnClickListener {
+            dialog.dismiss()
+
+            bottom_sheet_edit_password_cancel_button.setOnClickListener {
+                dialog.dismiss()
+            }
+
+            bottom_sheet_edit_password_save_button.setOnClickListener {
+
+                updateName(bottom_sheet_edit_password_edit_text.text.toString())
+                dialog.dismiss()
+            }
+        }
+        dialog.show()
+
+    }
 //    fun openGallery(){
 //
 //        val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
--- FixX/app/src/main/java/com/example/fixx/NavigationBar/SettingsScreen/SettingsFragment.kt
+++ FixX/app/src/main/java/com/example/fixx/NavigationBar/SettingsScreen/SettingsFragment.kt
@@ -1,14 +1,21 @@
 package com.example.project.bottom_navigation_fragments
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.PopupMenu
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import com.example.fixx.NavigationBar.SettingsScreen.HelpActivity
 import com.example.fixx.NavigationBar.SettingsScreen.ProfileActivity
 import com.example.fixx.R
+import com.example.fixx.RegistrationActivity
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -41,6 +48,43 @@ class SettingsFragment : Fragment() {
         // Inflate the layout for this fragment
         var rootView = inflater.inflate(R.layout.fragment_settings, container, false)
 
+
+
+//            val popupMenu = PopupMenu(getActivity(), settingsfragment_language_linear_layout)
+//            popupMenu.menuInflater.inflate(R.menu.language_popup, popupMenu.menu)
+
+
+
+       // val menuItemView = view!!.findViewById<View>(R.id.settingsfragment_language_linear_layout)
+
+
+//        val popupMenu = PopupMenu(activity, menuItemView)
+//        popupMenu.getMenuInflater().inflate(R.menu.language_popup, popupMenu.getMenu())
+//
+//            popupMenu.setOnMenuItemClickListener { menuItem ->
+//
+//                val id = menuItem.itemId
+//
+//                if (id == R.id.language_english) {
+//
+//                    println("english")
+//                } else if (id == R.id.language_arabic) {
+//
+//                    println("arabic")
+//                }
+//
+//                false
+//
+//            }
+//
+//
+//            settingsfragment_language_linear_layout.setOnClickListener {
+//                popupMenu.show()
+//            }
+
+
+
+
         val profile = rootView.findViewById(R.id.settingsfragment_profile_linear_layout) as LinearLayout
         profile.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View) {
@@ -48,12 +92,71 @@ class SettingsFragment : Fragment() {
                 openProfileActivity();
             }
         })
+
+        val language = rootView.findViewById(R.id.settingsfragment_language_linear_layout) as LinearLayout
+        language.setOnClickListener(object : View.OnClickListener {
+            @RequiresApi(Build.VERSION_CODES.KITKAT)
+            override fun onClick(v: View) {
+
+              //  val popupMenu = PopupMenu(getActivity(),view, Gravity.BOTTOM)
+//            popupMenu.menuInflater.inflate(R.menu.language_popup, popupMenu.menu)
+
+                val popupMenu = PopupMenu(getActivity(),settingsfragment_language_linear_layout)
+
+                popupMenu.getMenuInflater().inflate(R.menu.language_popup, popupMenu.getMenu())
+
+            popupMenu.setOnMenuItemClickListener { menuItem ->
+
+                val id = menuItem.itemId
+
+               if (id == R.id.language_english) {
+
+                   println("english")
+              } else if (id == R.id.language_arabic) {
+
+                    println("arabic")
+               }
+
+               false
+            }
+                             popupMenu.show()
+
+
+            }
+        })
+
+
+        val help = rootView.findViewById(R.id.settingsfragment_help_linear_layout) as LinearLayout
+        help.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+
+                openHelpActivity();
+            }
+        })
+
+        val exit = rootView.findViewById(R.id.settingsfragment_exit_linear_layout) as LinearLayout
+        exit.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                val intent = Intent (getActivity(), RegistrationActivity::class.java)
+                getActivity()?.startActivity(intent)
+
+            }
+        })
+
+
         return rootView
     }
 
     fun openProfileActivity(){
 
         val intent = Intent (getActivity(), ProfileActivity::class.java)
+        getActivity()?.startActivity(intent)
+
+    }
+
+    fun openHelpActivity(){
+
+        val intent = Intent (getActivity(), HelpActivity::class.java)
         getActivity()?.startActivity(intent)
 
     }
