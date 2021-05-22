f5836421e8cee7f3f81f616b16b1aa02938cb07e
MohamedHamedQaddal <mohamed_h_qaddal@yahoo.com>
2021-05-22 17:39:35 +0200

    pendingBidRecView

---
+++ FixX/app/src/main/java/com/example/fixx/JobDetailsDisplay/HorizontalScrollImageAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.fixx.JobDetailsDisplay
+
+/*
+import android.R
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+
+
+private class HorizontalScrollImageAdapter(ctx: Context?, imageModelArrayList: ArrayList<ImageView>)
+    : RecyclerView.Adapter<HorizontalScrollImageAdapter.ViewHolder>() {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(ctx)
+    private val imageModelArrayList: ArrayList<ImageView> = imageModelArrayList
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view: View = inflater.inflate(R.layout.recycler_image_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.imageView.setImageResource(imageModelArrayList[position].getImage_drawable())
+    }
+
+    override fun getItemCount(): Int {
+        return imageModelArrayList.size
+    }
+
+    internal inner class ViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+        var imageView: ImageView = itemView.findViewById<View>(R.id.imageView) as ImageView
+    }
+}*/
